1415c82528d039ef32695c36296f66daf501ffef
alexgagnon <asgagnon@hotmail.com>
2019-01-31 17:11:00 -0500

    not ready for api gen

--- packages/cli/cli-generate-api.js
+++ packages/cli/cli-generate-api.js
@@ -2,15 +2,16 @@ const cli = require('commander');
 
 cli
 .name('api-tools generate api')
-.description(
-  'Generate full or partial api source code stubs, currently uses Gradle and Spring with Spring Boot. If none of the specified -c/s/m/u options are provided, it will generate the entire project'
-)
-.option(
-  '-p, --package <package>',
-  'defines the \'.\'-separated packaging structure (e.g. api.canada.ca)'
-)
-.option('-c, --controllers')
-.option('-S, --services')
-.option('-m, --models')
-.option('-u, --utils')
+.description('TODO?')
+// .description(
+//   'Generate full or partial api source code stubs, currently uses Gradle and Spring with Spring Boot. If none of the specified -c/s/m/u options are provided, it will generate the entire project'
+// )
+// .option(
+//   '-p, --package <package>',
+//   'defines the \'.\'-separated packaging structure (e.g. api.canada.ca)'
+// )
+// .option('-c, --controllers')
+// .option('-S, --services')
+// .option('-m, --models')
+// .option('-u, --utils')
 .parse(process.argv);
